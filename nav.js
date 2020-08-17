3518042c28965ebce0544d51c347f906ffc6a2b4
RF604 <raffi@freedman.ca>
2020-08-17 16:01:13 -0700

    periodic commit

--- nav.js
+++ nav.js
@@ -1,25 +1,17 @@
 const navLinks = document.querySelectorAll("nav a")
-const sections = document.querySelectorAll("aside section")
-const navObserver = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.intersectionRatio > 0.50) {
-      sections.forEach((section, index) => {
-        if (entry.target == section) {
-          navLinks.forEach((link, i) => {
-            if (index === i) {
-              navLinks[index].classList.add("selected")
-            } else {
-              link.classList.remove("selected")
-            }
-          })
-        }
-      })
-    }
+const sections = document.querySelectorAll("aside section.project")
+sections.forEach((section, index) => {
+  const link = navLinks[index]
+  const obs = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.intersectionRatio > 0.50) {
+        link.classList.add("selected")
+      } else {
+        link.classList.remove("selected")
+      }
+    })
+  }, {
+    threshold: [0, 0.5, 1]
   })
-}, {
-  threshold: [0, 0.25, 0.5, 0.75, 1]
-})
-
-sections.forEach(section => {
-  navObserver.observe(section)
+  obs.observe(section)
 })
