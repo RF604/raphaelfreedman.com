2b27fe24b0b00dfebe1d5a97757f9f1ea6b62f17
RF604 <raffi@freedman.ca>
2020-08-19 21:32:58 -0700

    periodic commit

--- nav.js
+++ nav.js
@@ -4,14 +4,14 @@ sections.forEach((section, index) => {
   const link = navLinks[index]
   const obs = new IntersectionObserver(entries => {
     entries.forEach(entry => {
-      if (entry.intersectionRatio > 0.50) {
+      if (entry.intersectionRatio > 0.20) {
         link.classList.add("selected")
       } else {
         link.classList.remove("selected")
       }
     })
   }, {
-    threshold: [0, 0.5, 1]
+    threshold: [0, .2, .8, 1]
   })
   obs.observe(section)
 })
