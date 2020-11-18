28caf2a56b12cfd65010c8e55e6b9142280c10d8
RF604 <raffi@freedman.ca>
2020-11-18 09:17:42 -0800

    periodic commit

--- hover.js
+++ hover.js
@@ -2,24 +2,27 @@
 let photocontainer = document.querySelector("aside img")
 let imagelinks = document.querySelectorAll('.imagelink')
 
-imagelinks.forEach(link => {
-
 if (window.innerWidth > 1150) {
-let preload = new Image()
-preload.src = link.getAttribute('data-image')
+
+  imagelinks.forEach(link => {
+
+    let preload = new Image()
+    preload.src = link.getAttribute('data-image')
+
+
+    link.addEventListener("mouseenter", function() {
+      let photo = this.getAttribute('data-image');
+      photocontainer.src = photo;
+      photocontainer.style.backgroundColor = '#E5E7E5';
+      event.stopPropagation()
+    })
+
+    link.addEventListener("mouseleave", function() {
+      photocontainer.src = '/images/sutro.png';
+      photocontainer.style.backgroundColor = 'white';
+      event.stopPropagation()
+    })
+
+  })
+
 }
-
-link.addEventListener("mouseenter", function() {
-    let photo = this.getAttribute('data-image');
-    photocontainer.src = photo;
-    photocontainer.style.backgroundColor = '#E5E7E5';
-    event.stopPropagation()
-  })
-
-  link.addEventListener("mouseleave", function() {
-    photocontainer.src = '/images/sutro.png';
-    photocontainer.style.backgroundColor = 'white';
-    event.stopPropagation()
-  })
-
-})
