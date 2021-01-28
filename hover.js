190bc3a37723cbc8d39d7830bf81ca49cc74a840
RF604 <raffi@freedman.ca>
2021-01-28 12:03:35 -0800

    periodic commit

--- hover.js
+++ hover.js
@@ -1,4 +1,5 @@
 
+
 let photocontainer = document.querySelector('aside img')
 let imagelinks = document.querySelectorAll('.imagelink')
 
@@ -10,21 +11,18 @@ if (window.innerWidth > 1150) {
     preload.src = link.getAttribute('data-image')
 
     link.addEventListener("mouseenter", function() {
-
       let photo = this.getAttribute('data-image');
       let background = this.getAttribute('data-background');
-
       photocontainer.src = photo;
       photocontainer.style.backgroundImage = `url(${background})`;
-
-      photocontainer.classList.add("hover")
+      photocontainer.classList.add("hover");
       event.stopPropagation()
     })
 
     link.addEventListener("mouseleave", function() {
       photocontainer.src = '/images/sutro.png';
       photocontainer.style.backgroundImage = 'none';
-      photocontainer.classList.remove("hover")
+      photocontainer.classList.remove("hover");
       event.stopPropagation()
     })
 
