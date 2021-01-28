eed6679dc4eb94e2841db36d2d59dab7cee03459
RF604 <raffi@freedman.ca>
2021-01-28 11:13:31 -0800

    periodic commit

--- hover.js
+++ hover.js
@@ -12,15 +12,13 @@ if (window.innerWidth > 1150) {
     link.addEventListener("mouseenter", function() {
       let photo = this.getAttribute('data-image');
       photocontainer.src = photo;
-      photocontainer.style.backgroundColor = '#E5E5E5';
-      photocontainer.style.backgroundImage = 'none';
+      photocontainer.classList.add("hover")
       event.stopPropagation()
     })
 
     link.addEventListener("mouseleave", function() {
       photocontainer.src = '/images/sutro.png';
-      photocontainer.style.backgroundColor = 'white';
-      photocontainer.style.backgroundImage = 'none';
+      photocontainer.classList.remove("hover")
       event.stopPropagation()
     })
 
